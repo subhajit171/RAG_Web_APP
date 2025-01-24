c2f701c9d84d18c7ac1cfb3bdbba35b6f283adfd
skg171 <subhajitgayen111@gmail.com>
2025-01-24 17:37:34 +0530

    2nd commit

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+st.secrects["HF_TOKEN"]
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
