d9492c08277b75ab5cbc3a8afbdeb31ba634f159
skg171 <subhajitgayen111@gmail.com>
2025-01-24 18:38:57 +0530

    13

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import chromadb
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ['HF_TOKEN']=st.secrects["HF_TOKEN"]
+os.environ['HF_TOKEN']=st.secrets["HF_TOKEN"]
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
